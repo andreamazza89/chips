c6bee3b57aaae0fbd3c6a4ba56379b74425e6704
andreamazza89 <andrea.mazzarella89@gmail.com>
2018-01-12 17:33:17 +0000

    uses elm program

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -3,6 +3,31 @@ module Main exposing (..)
 import Html exposing (Html, text)
 
 
-main : Html a
+type alias Model =
+    {}
+
+
+type Msg
+    = Nothing
+
+
+main : Program Never Model Msg
 main =
-    text "Hello, World!"
+    Html.program
+        { init = ( {}, Cmd.none )
+        , update = update
+        , subscriptions = (always Sub.none)
+        , view = view
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Nothing ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    text "Ciao Mondo"
