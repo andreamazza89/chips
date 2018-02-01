e0d33e7bd110d82e98291edeea0463931099e173
andreamazza89 <andrea.mazzarella89@gmail.com>
2018-02-01 22:11:46 +0100

    tidy up queries

--- assets/elm/Queries.elm
+++ assets/elm/Queries.elm
@@ -1,7 +1,7 @@
 module Queries exposing (..)
 
 import Http exposing (..)
-import Json.Decode exposing (list, field, float, map2, map3, map4, string, int)
+import Json.Decode exposing (list, field, float, map, map2, map3, map4, string, int)
 import Json.Encode exposing (encode, object)
 import Data exposing (..)
 
@@ -12,7 +12,7 @@ createTournamentSeries model =
         Http.post
             "http://localhost:4000/api"
             (newTournamentSeriesRequestBody model.formData.tournamentSeries.city model.formData.tournamentSeries.name)
-            yetAnotherDecoder
+            (graphQlDecoder "createTournamentSeries" (list tournamentSeriesDecoder))
 
 
 newTournamentSeriesRequestBody : String -> String -> Body
@@ -34,13 +34,6 @@ newTournamentSeriesRequestBody city name =
         )
 
 
-yetAnotherDecoder : Json.Decode.Decoder (List TournamentSeries)
-yetAnotherDecoder =
-    field "data" <|
-        field "createTournamentSeries" <|
-            Json.Decode.list tournamentSeriesDecoder
-
-
 createNewStakingContract : Model -> TournamentId -> Cmd Msg
 createNewStakingContract model tournamentId =
     Http.send UpdateTournamentSeriesesShow <|
@@ -53,7 +46,7 @@ createNewStakingContract model tournamentId =
                 model.formData.stakingContract.rate
                 tournamentId
             )
-            tournamentMutationDecoder
+            (graphQlDecoder "createStakingContract" (list tournamentSeriesDecoder))
 
 
 newStakeContractRequestBody : String -> Int -> String -> Float -> String -> Body
@@ -87,7 +80,7 @@ createTournament model seriesId =
         Http.post
             "http://localhost:4000/api"
             (newTournamentRequestBody model.formData.tournament.name model.formData.tournament.feeInCents seriesId)
-            seriesMutationDecoder
+            (graphQlDecoder "createTournament" (list tournamentSeriesDecoder))
 
 
 newTournamentRequestBody : String -> Int -> SeriesId -> Body
@@ -113,21 +106,11 @@ newTournamentRequestBody name feeInCents seriesId =
 
 fetchUsers : Cmd Msg
 fetchUsers =
-    Http.send UpdateUsersShown <| Http.post "http://localhost:4000/api" usersRequestBody usersDecoder
-
-
-usersDecoder : Json.Decode.Decoder (List User)
-usersDecoder =
-    field "data" <|
-        field "users" <|
-            Json.Decode.list userDecoder
-
-
-userDecoder : Json.Decode.Decoder User
-userDecoder =
-    map2 User
-        (field "id" string)
-        (field "name" string)
+    Http.send UpdateUsersShown <|
+        Http.post
+            "http://localhost:4000/api"
+            usersRequestBody
+            (graphQlDecoder "users" (list userDecoder))
 
 
 usersRequestBody : Body
@@ -140,7 +123,11 @@ usersRequestBody =
 
 fetchSerieses : Cmd Msg
 fetchSerieses =
-    Http.send UpdateTournamentSeriesesShow <| Http.post "http://localhost:4000/api" tournamentSeriesesRequestBody tournamentSeriesesDecoder
+    Http.send UpdateTournamentSeriesesShow <|
+        Http.post
+            "http://localhost:4000/api"
+            tournamentSeriesesRequestBody
+            (graphQlDecoder "tournamentSerieses" (list tournamentSeriesDecoder))
 
 
 tournamentSeriesesRequestBody : Body
@@ -160,25 +147,35 @@ tournamentSeriesesRequestBody =
         )
 
 
-seriesMutationDecoder : Json.Decode.Decoder (List TournamentSeries)
-seriesMutationDecoder =
+createUser : String -> String -> Cmd Msg
+createUser name email =
+    Http.send UpdateUsersShown <|
+        Http.post
+            "http://localhost:4000/api"
+            (createUserRequestBody name email)
+            (graphQlDecoder "createUser" (list userDecoder))
+
+
+createUserRequestBody : String -> String -> Body
+createUserRequestBody name email =
+    Http.jsonBody
+        (Json.Encode.object
+            [ ( "query", Json.Encode.string ("mutation createUser { createUser(name: \"" ++ name ++ "\", email: \"" ++ email ++ "\") { id, name } }") ) ]
+        )
+
+
+graphQlDecoder : String -> Json.Decode.Decoder a -> Json.Decode.Decoder a
+graphQlDecoder fieldName decoder =
     field "data" <|
-        field "createTournament" <|
-            Json.Decode.list tournamentSeriesDecoder
+        field fieldName <|
+            decoder
 
 
-tournamentMutationDecoder : Json.Decode.Decoder (List TournamentSeries)
-tournamentMutationDecoder =
-    field "data" <|
-        field "createStakingContract" <|
-            Json.Decode.list tournamentSeriesDecoder
-
-
-tournamentSeriesesDecoder : Json.Decode.Decoder (List TournamentSeries)
-tournamentSeriesesDecoder =
-    field "data" <|
-        field "tournamentSerieses" <|
-            Json.Decode.list tournamentSeriesDecoder
+userDecoder : Json.Decode.Decoder User
+userDecoder =
+    map2 User
+        (field "id" string)
+        (field "name" string)
 
 
 tournamentSeriesDecoder : Json.Decode.Decoder TournamentSeries
@@ -187,14 +184,7 @@ tournamentSeriesDecoder =
         (field "city" string)
         (field "id" string)
         (field "name" string)
-        (field "tournaments" (Json.Decode.list tournamentDecoder))
-
-
-tournamentsDecoder : Json.Decode.Decoder (List Tournament)
-tournamentsDecoder =
-    field "data" <|
-        field "tournaments" <|
-            Json.Decode.list tournamentDecoder
+        (field "tournaments" (list tournamentDecoder))
 
 
 tournamentDecoder : Json.Decode.Decoder Tournament
@@ -202,7 +192,7 @@ tournamentDecoder =
     map3 Tournament
         (field "name" string)
         (field "id" string)
-        (field "stakingContracts" (Json.Decode.list stakingContractDecoder))
+        (field "stakingContracts" (list stakingContractDecoder))
 
 
 stakingContractDecoder : Json.Decode.Decoder StakingContract
@@ -215,25 +205,5 @@ stakingContractDecoder =
 
 stakerDecoder : Json.Decode.Decoder Staker
 stakerDecoder =
-    Json.Decode.map Staker
+    map Staker
         (field "name" string)
-
-
-createUser : String -> String -> Cmd Msg
-createUser name email =
-    Http.send UpdateUsersShown <| Http.post "http://localhost:4000/api" (createUserRequestBody name email) usersMutationDecoder
-
-
-createUserRequestBody : String -> String -> Body
-createUserRequestBody name email =
-    Http.jsonBody
-        (Json.Encode.object
-            [ ( "query", Json.Encode.string ("mutation createUser { createUser(name: \"" ++ name ++ "\", email: \"" ++ email ++ "\") { id, name } }") ) ]
-        )
-
-
-usersMutationDecoder : Json.Decode.Decoder (List User)
-usersMutationDecoder =
-    field "data" <|
-        field "createUser" <|
-            Json.Decode.list userDecoder
